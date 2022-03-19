dcc49c4dc403d0762a0a26ec84a87456f54a6b75
kylee <leeky940926@gmail.com>
2022-03-19 13:54:27 +0900

    Initialize Channels

--- channels/settings.py
+++ channels/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'channels.wsgi.application'
+ASGI_APPLICATION = 'channels.asgi.application'
 
 
 # Database
